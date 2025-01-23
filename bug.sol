6f3fe3a7258db4af08f2ad4d227bc796d6b216f5
BugHunter-X <askzenai@gmail.com>
2025-01-23 15:44:04 +0100

    Add bug.sol file

---
+++ bug.sol
@@ -0,0 +1,7 @@
+```solidity
+function withdraw(uint256 amount) public {
+    require(balances[msg.sender] >= amount, "Insufficient balance");
+    balances[msg.sender] -= amount;
+    payable(msg.sender).transfer(amount);
+}
+```
